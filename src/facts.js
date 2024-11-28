157e3626b7b8ae8dc68b1421d8b8fa96708bfdd2
Callum Inglis <callum.inglis@4oh4.co.uk>
2024-11-28 16:01:25 +0000

    typos

--- src/facts.js
+++ src/facts.js
@@ -1,6 +1,7 @@
 const facts = [
+    "Women typically invest less than men",
     "50 years ago, my nan couldn't get a bank account in her own name",
-    "My gran got paid less than a mn for doing the exact same job",
+    "My gran got paid less than a man for doing the exact same job",
     "The Gender Pension Gap belongs in the past",
     "No man meant no mortgage for my gran in 1974",
     "Shared parental leave wasn't a thing when my mum had me",
